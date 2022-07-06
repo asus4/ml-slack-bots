e1821dea79e0cc7b3e5c52e17640a822d9b60bd5
Koki Ibukuro <koki.ibukuro@gmail.com>
2022-07-06 09:27:20 +0200

    Add OpenAI API key

--- app/config.py
+++ app/config.py
@@ -2,7 +2,9 @@ import os
 
 
 class Config:
+    # Load Tokens from .env file
     HUGGINGFACE_API_TOKEN = os.environ["HUGGINGFACE_API_TOKEN"]
     REPLICATE_API_TOKEN = os.environ["REPLICATE_API_TOKEN"]
+    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
     SLACK_BOT_OAUTH_TOKEN = os.environ["SLACK_BOT_OAUTH_TOKEN"]
     SLACK_VERIFICATION_TOKEN = os.environ["SLACK_VERIFICATION_TOKEN"]
